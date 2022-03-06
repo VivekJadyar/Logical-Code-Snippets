718e08ea02e9871393665715d1947d4ca3acf6ab
VivekJadyar <100848524+VivekJadyar@users.noreply.github.com>
2022-03-07 00:09:28 +0530

    Assignment no. 3 __ Day 5 __ Pattern1 _ 05.03.2022

---
+++ Day5_Pattern_1/P2.java
@@ -0,0 +1,20 @@
+
+class P2 {
+	
+	public static void main(String args []) {
+		
+		for (char rows = 'A'; rows <= 'E'; rows++) {
+			
+			for (char column = 'A'; column <= rows; column++){
+				
+				System.out.print(column + " ");
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}	
+	
+}
