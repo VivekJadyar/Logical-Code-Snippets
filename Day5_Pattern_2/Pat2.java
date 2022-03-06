7325ce0f45ec921775247e21fea9b8849aeb43c8
VivekJadyar <100848524+VivekJadyar@users.noreply.github.com>
2022-03-06 22:18:43 +0530

    Assignment no. 3 __ Day 5 __Pattern2 __ 05.03.2022

---
+++ Day5_Pattern_2/Pat2.java
@@ -0,0 +1,26 @@
+
+class Pat2 {
+	
+	public static void main(String args []) {
+		
+		for (int rows = 1; rows <= 5; rows++) {
+			
+			for (int space = 5 - rows; space >= 1; space--){
+				
+				System.out.print(" ");
+				
+			}
+			
+			for (int column = 1; column <= rows; column++){
+				
+				System.out.print("*");
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}	
+	
+}
---
+++ Day5_Pattern_2/Pat3.java
@@ -0,0 +1,20 @@
+
+class Pat3 {
+	
+	public static void main(String args []) {
+		
+		for (int rows = 1; rows <= 5; rows++) {
+			
+			for (int column = 5 - rows; column >= 1; column--){
+				
+				System.out.print("*");
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}	
+	
+}
